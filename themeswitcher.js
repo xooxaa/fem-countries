4233d68d7bc4443a569511e6a9352fa6ba494586
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-12-06 22:23:48 +0100

    Layout main page finished dark mode switch working

---
+++ themeswitcher.js
@@ -0,0 +1,48 @@
+//determines if the user has a set theme
+function detectColorScheme() {
+  var theme = "light"; //default to light
+
+  //local storage is used to override OS theme settings
+  if (localStorage.getItem("theme")) {
+    if (localStorage.getItem("theme") == "dark") {
+      var theme = "dark";
+    }
+  } else if (!window.matchMedia) {
+    //matchMedia method not supported
+    return false;
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    //OS theme setting detected as dark
+    var theme = "dark";
+  }
+
+  //dark theme preferred, set document with a `data-theme` attribute
+  if (theme == "dark") {
+    document.documentElement.setAttribute("dataTheme", "dark");
+  }
+}
+detectColorScheme();
+
+//identify the toggle switch HTML element
+const toggleSwitch = document.querySelector(".color-theme");
+let darkmode = false;
+
+//function that changes the theme, and sets a localStorage variable to track the theme between page loads
+function switchTheme(e) {
+  if (darkmode) {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("dataTheme", "light");
+    darkmode = false;
+  } else {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("dataTheme", "dark");
+    darkmode = true;
+  }
+}
+
+//listener for changing themes
+toggleSwitch.addEventListener("click", switchTheme, false);
+
+//pre-check the dark-theme checkbox if dark-theme is set
+if (document.documentElement.getAttribute("dataTheme") == "dark") {
+  darkmode = true;
+}
