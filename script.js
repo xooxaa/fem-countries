9df0ae2669d72b8170221c8af7618375e0f4c978
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-12-15 18:42:19 +0100

    Loading countries form local storage when available Filling Country cards on load Region and Population Filter working

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //******************************************************************************
 // variable and constant declaration
+const header = document.querySelector("header");
 const results = document.querySelector(".countries");
+const filter = document.querySelector(".filter");
+const countryDetail = document.querySelector(".country-detail");
+const backBtn = document.querySelector(".back-btn");
+const searchInput = document.querySelector(".country-input");
+const sortSelect = document.querySelector(".sort-select");
+const regionsSelect = document.querySelector(".regions-select");
+const populationSelect = document.querySelector(".population-select");
 let countries = [];
 
 //******************************************************************************
@@ -27,6 +35,7 @@ const fetchCountries = function () {
           population: country.population,
           region: country.region,
           subregion: country.subregion,
+          flag: country.flags.png,
           toplevel: country.tld[0],
           currencies: [],
           languages: [...Object.values(country.languages)],
@@ -41,7 +50,9 @@ const fetchCountries = function () {
           }
         }
       }
-      console.log(countries);
+      localStorage.setItem("countries", JSON.stringify(countries));
+      console.log("succesfully fetched countries from api", countries);
+      fillCountries();
     })
     .catch((err) => {
       console.log(err);
@@ -52,7 +63,34 @@ const fetchCountries = function () {
 // sort countries by name
 // sort countries by populations
 // sort countries by capital
+
 // append country cards to DOM
+const fillCountries = function () {
+  for (let country of countries) {
+    const newDivCountry = document.createElement("div");
+    const newImgFlag = document.createElement("img");
+    const newDivInfo = document.createElement("div");
+    // const newH2 = document.createElement("h2");
+    // const newPopulation = document.createElement("p");
+    // const newPRegion = document.createElement("p");
+    // const newCapital = document.createElement("p");
+    newDivCountry.classList.add("country");
+    newImgFlag.src = country.flag;
+    newDivInfo.classList.add("infos");
+    newDivInfo.innerHTML = `
+      <h2>${country.name}</h2>
+      <p>Population: <span>${country.population.toLocaleString()}</span></p>
+      <p>Region: <span>${country.region}</span></p>
+      <p>Capital: <span>${country.capital}</span></p>
+    `;
+
+    newDivCountry.append(newImgFlag);
+    newDivCountry.append(newDivInfo);
+
+    results.append(newDivCountry);
+  }
+};
+
 // <div class="country">
 //   <img src="./ger.webp" alt="German Flag" />
 //   <div class="infos">
@@ -68,39 +106,68 @@ const fetchCountries = function () {
 // go through all countries and apply hidden class to those who dont match query
 
 //******************************************************************************
-// filter by region
-// go through all countries and apply hidden class to those who dont match region
-// filter by population
-// go through all countries and apply hidden class to those who have less than x inheritants
+// filter by region and/or population
+const filterCountries = function () {
+  // go through all countries and apply hidden class to those who dont match criteria
+  for (let countryDiv of results.children) {
+    countryDiv.classList.remove("hidden");
+    const region = countryDiv.children[1].children[2].children[0].textContent;
+    const population = Number(
+      countryDiv.children[1].children[1].children[0].textContent
+        .replaceAll(".", "")
+        .replaceAll(",", "")
+    );
+    if (
+      regionsSelect.value &&
+      !(region.toLowerCase() === regionsSelect.value.toLowerCase())
+    ) {
+      countryDiv.classList.add("hidden");
+    }
+    populationCap = Number(populationSelect.value);
+    if (population < populationCap) {
+      countryDiv.classList.add("hidden");
+    }
+  }
+};
+regionsSelect.addEventListener("change", () => {
+  filterCountries();
+});
+populationSelect.addEventListener("change", () => {
+  filterCountries();
+});
 
 //******************************************************************************
 // event listeners
 // debug buttons
-const debugFetch = document.querySelector(".debug-fetch");
-const debugClear = document.querySelector(".debug-clear");
-const debugStore = document.querySelector(".debug-store");
-const debugRecall = document.querySelector(".debug-recall");
-
-debugFetch.addEventListener("click", () => {
-  fetchCountries();
-});
-
-debugClear.addEventListener("click", () => {
-  localStorage.removeItem("countries");
-});
-
-debugStore.addEventListener("click", () => {
-  localStorage.setItem("countries", JSON.stringify(countries));
-});
-
-debugRecall.addEventListener("click", () => {
-  // JSON.parse(JSON.stringify(nestedArray));
-  countries = JSON.parse(localStorage.getItem("countries"));
-  console.log(countries);
-});
 
 // clicking on a country reveals detail information and hides filter and countries
+results.addEventListener("click", () => {
+  results.classList.add("hidden");
+  filter.classList.add("hidden");
+  countryDetail.classList.remove("hidden");
+  backBtn.scrollIntoView();
+  header.scrollIntoView();
+});
 // back button hides detail information and shows filter and countries
+backBtn.addEventListener("click", () => {
+  results.classList.remove("hidden");
+  filter.classList.remove("hidden");
+  countryDetail.classList.add("hidden");
+  filter.scrollIntoView();
+  header.scrollIntoView();
+});
 
 //******************************************************************************
 // initialy check if countries are already locally stored then recall else fetch
+if (!JSON.parse(localStorage.getItem("countries"))) {
+  fetchCountries();
+} else {
+  countries = JSON.parse(localStorage.getItem("countries"));
+  console.log("loaded countries from local storage", countries);
+  fillCountries();
+}
+
+searchInput.value = "";
+regionsSelect.selectedIndex = 0;
+populationSelect.selectedIndex = 0;
+sortSelect.selectedIndex = 0;
